e94f6614937a6cd7b941fcc5f1c70f6d8c699335
clivejj <cjohnston1@stuy.edu>
2017-10-09 12:25:04 -0400

    finish

--- blimpo.py
+++ blimpo.py
@@ -1,5 +1,5 @@
-import random, hashlib, os
-from flask import Flask, render_template, request, session
+from os import urandom
+from flask import Flask, render_template, request, session, redirect, url_for
 
 #set default user/password for testing
 defaultUser = "Bob"
@@ -7,36 +7,46 @@ defaultPassword = "123"
 
 #create the app and add a session/cookie
 app=Flask(__name__)
-app.secret_key=os.urandom(32)
+app.secret_key=urandom(32)
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/')
 def root():
-	#render loggedIn page if session has "user":"Bob"
-	if len(session)==1 and  session["user"]=="Bob":
-		return render_template("loggedIn.html")
-        #otherwise, render login page
-	else:
-                return render_template("login.html")
+    #render loggedIn page if session has "user":"Bob"
+    if (len(session)==1 and session["user"]=="Bob"):
+        return redirect(url_for("welcome"))
+    #otherwise, render login page
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/entry", methods=["GET", "POST"])	      
 def entry():
-        #if correct info is entered thru form, store session, and render loggedIn page
-        if (request.form["username"]=="Bob") and (request.form["password"]=="123"):
-                session["user"] = "Bob"
-                return render_template("loggedIn.html")
-        #otherwise if username/password combo is wrong, render error page
-        else:
-                return render_template("error.html")
+    #if correct info is entered thru form, store session, and render loggedIn page
+    if (request.form["username"]=="Bob") and (request.form["password"]=="123"):
+        session["user"] = "Bob"
+        return redirect(url_for("welcome"))
+    #otherwise if username/password combo is wrong, render error page
+    else:
+        return redirect(url_for("error"))
 
 @app.route("/logOff", methods=["GET", "POST"])
 def logOff():
-        #if logoff button is hit, remove session, and render logoff page
-        session.pop("user")
-        return render_template("loggedOff.html")
+    #if logoff button is hit, remove session, and render logoff page
+    session.pop("user")
+    return redirect(url_for("login"))
         
 
-if __name__ == '__main__':
-	app.debug==True
-	app.run()	
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
 
-	
+@app.route('/login')
+def login():
+    return render_template("login.html")
+    
+@app.route('/error')
+def error():
+    return render_template("error.html")
+
+if __name__ == '__main__':
+    app.debug==True
+    app.run()	
